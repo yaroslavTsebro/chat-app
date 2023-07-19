cb6a8e7087a6cde08171ab501491b71728f59ac9
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-07-19 13:01:44 +0300

    added routes

--- backend/src/route/index.ts
+++ backend/src/route/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import {userRouter} from "./user";
 
 let router = express.Router();
+router.use('/user', userRouter);
 
 export { router as mainRouter };
 
---
+++ backend/src/route/user.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import userController from '../controller/user-controller';
+import { authorizationMiddleware } from '../middleware/authorization-middleware';
+
+let router = express.Router();
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', authorizationMiddleware, userController.logout);
+router.post('/activate/:link', userController.activate);
+router.post('/send-otp', authorizationMiddleware, userController.sendOtp);
+router.get('/refresh', userController.refresh);
+
+export { router as userRouter };
