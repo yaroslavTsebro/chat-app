9e035c1973c51dcbc163a7d32ec86eaaccbec7ef
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-07-19 12:52:42 +0300

    added jwt utility

--- backend/src/utility/jwt.ts
+++ backend/src/utility/jwt.ts
@@ -1,32 +1,54 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { config } from '../config/config';
 import { TokenPayload } from '../entity/dto/token/token-payload';
+import { OtpPayload } from '../entity/dto/otp/otp-payload';
 
 class Jwt {
   constructor(
     private readonly accessExpires = config.server.jwt.expiresInAccess,
     private readonly refreshExpires = config.server.jwt.expiresInRefresh,
     private readonly accessSecret = config.server.jwt.accessSecret,
-    private readonly refreshSecret = config.server.jwt.refreshSecret
+    private readonly refreshSecret = config.server.jwt.refreshSecret,
+    private readonly otpSecret = config.server.otp.secret
   ) {}
 
   public generateTokens(token: TokenPayload): {
     accessToken: string;
     refreshToken: string;
   } {
-    const accessToken: string = jwt.sign({ ...token }, this.accessSecret, {
-      expiresIn: this.accessExpires,
-    });
-    const refreshToken: string = jwt.sign({ ...token }, this.refreshSecret, {
-      expiresIn: this.refreshExpires,
-    });
+    const accessToken = this.generateToken(
+      token,
+      this.accessSecret,
+      this.accessExpires
+    );
+
+    const refreshToken = this.generateToken(
+      token,
+      this.refreshSecret,
+      this.refreshExpires
+    );
+
     return {
       accessToken,
       refreshToken,
     };
   }
 
+  public generateToken(
+    token: TokenPayload | OtpPayload,
+    secret: string,
+    expirationTime: string
+  ): string {
+    return jwt.sign({ ...token }, secret, {
+      expiresIn: expirationTime,
+    });
+  }
+
   public validateAccessToken(token: string): JwtPayload | string | null {
+    return this.validateToken(token, this.accessSecret);
+  }
+
+  public validateOtp(token: string): JwtPayload | string | null {
     return this.validateToken(token, this.accessSecret);
   }
 
