ef437b610b98ef97b15673af004294c4c00b1724
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-02 14:36:55 +0300

    added validation for files

--- backend/src/service/service-helper.ts
+++ backend/src/service/service-helper.ts
@@ -1,35 +1,60 @@
-import {AppError} from "../entity/error/app-error";
-import {ErrorCodes} from "../constant/error-codes";
-import {ErrorMessages} from "../constant/error-messages";
-import logger from "../utility/logger";
-import {validateSync} from "class-validator";
+import { AppError } from '../entity/error/app-error';
+import { ErrorCodes } from '../constant/error-codes';
+import { ErrorMessages } from '../constant/error-messages';
+import logger from '../utility/logger';
+import { validateSync } from 'class-validator';
+import { UploadedFile } from 'express-fileupload';
+import groupRepository from '../repository/group-repository';
+import { IGroup } from '../entity/db/model/group';
 
 export class ServiceHelper {
   public static validateDto<T extends object>(dto: T): void {
-    const errors = validateSync(dto, {enableDebugMessages: false});
+    const errors = validateSync(dto, { enableDebugMessages: false });
     if (errors.length > 0) {
       throw new AppError(
-          ErrorCodes.VALIDATION_ERROR,
-          ErrorMessages.VALIDATION_ERROR, errors.map(e => {
-            if (e.constraints) {
-              return e.constraints;
-            }
-          }))
+        ErrorCodes.VALIDATION_ERROR,
+        ErrorMessages.VALIDATION_ERROR,
+        errors.map((e) => {
+          if (e.constraints) {
+            return e.constraints;
+          }
+        })
+      );
     }
-    logger.info("Validated successfully");
+    logger.info('Validated successfully');
   }
 
   public static validateId(id: string): void {
     if (id.length < 12) {
-      throw new AppError(
-          ErrorCodes.BAD_REQUEST,
-          ErrorMessages.BAD_REQUEST,
-          []);
+      throw new AppError(ErrorCodes.BAD_REQUEST, ErrorMessages.BAD_REQUEST, []);
     }
-    logger.info("Validated successfully")
+    logger.info('Validated successfully');
   }
 
   public static validateIds(ids: string[]): void {
-    ids.map(id => this.validateId(id));
+    ids.map((id) => this.validateId(id));
+  }
+
+  public static checkFilesForPresence(img: UploadedFile | undefined): void {
+    if (img == undefined) {
+      throw new AppError(ErrorCodes.BAD_REQUEST, ErrorMessages.BAD_REQUEST);
+    }
+  }
+
+  public static checkFiles(img: UploadedFile[] | undefined): void {
+    if (img == undefined) {
+      throw new AppError(ErrorCodes.BAD_REQUEST, ErrorMessages.BAD_REQUEST);
+    }
+  }
+
+  public static async checkUserForGroupOwner(
+    groupId: string,
+    userId: string
+  ): Promise<IGroup> {
+    const group = await groupRepository.getGroupByIdAndOwnerId(userId, groupId);
+    if (!group) {
+      throw new AppError(ErrorCodes.FORBIDDEN, ErrorMessages.FORBIDDEN);
+    }
+    return group
   }
 }
