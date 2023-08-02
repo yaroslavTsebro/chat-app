ffa0026d3438f5de855f7d84ccc485999f554e98
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-02 14:39:09 +0300

    added logging

--- backend/src/service/otp-service.ts
+++ backend/src/service/otp-service.ts
@@ -1,27 +1,30 @@
 import otpRepository from '../repository/otp-repository';
-import * as uuid from 'uuid';
 import { config } from '../config/config';
 import Jwt from '../utility/jwt';
 import { IOtp } from '../entity/db/model/otp';
 import { OtpPayload } from '../entity/dto/otp/otp-payload';
+import logger from '../utility/logger';
 
 class OtpService {
+
   public async createOtp(userId: string): Promise<IOtp> {
     try {
       await otpRepository.findAndDeleteByUserId(userId);
       const payload = new OtpPayload();
       payload.userId = userId;
 
-      let token = Jwt.generateToken(
+      const token = Jwt.generateToken(
         payload,
         config.server.otp.secret,
         config.server.otp.expires
       );
       return await otpRepository.create(userId, token);
     } catch (e) {
+      logger.error("An error occurred in token service")
       throw e;
     }
   }
+
 }
 
 export default new OtpService();
