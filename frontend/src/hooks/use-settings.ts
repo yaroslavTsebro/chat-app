f2d5a05428211b6a8ddb5cd84ff2f1afd61c4d68
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-14 16:10:08 +0300

    added use-settings

---
+++ frontend/src/hooks/use-settings.ts
@@ -0,0 +1,26 @@
+import { DEFAULT_SETTINGS } from "../const/default-settings";
+import { Settings } from "../entity/model/settings";
+import { ThemeEnum } from "../entity/model/theme-enum";
+
+export function useSettings(): [Settings, (settings: Settings) => void] {
+  const [settings, setSettings] = useLocalStorage<Settings>(
+    "settings_V3",
+    {} as Settings
+  );
+
+  const settingsCopy: Settings = {
+    ...DEFAULT_SETTINGS,
+    ...settings,
+    theme: parseTheme(settings.themeRaw ?? DEFAULT_SETTINGS.theme),
+  };
+
+  return [settingsCopy, setSettings];
+}
+
+function parseTheme(theme: ThemeEnum) {
+  if (theme === ThemeEnum.Auto) {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    return darkThemeMq.matches ? ThemeEnum.Dark : ThemeEnum.Light;
+  }
+  return theme;
+}
